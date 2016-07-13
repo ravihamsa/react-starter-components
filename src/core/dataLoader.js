18a722fc27c766e31f33640b51453437ffef2f0e
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-07-13 16:30:11 +0530

    fixed case issue in dataLoader

--- src/core/dataLoader.js
+++ src/core/dataLoader.js
@@ -71,7 +71,7 @@ class DataLoader {
                 credentials: 'include'
             }
 
-            config.method === 'post' ? requestConfig.body = JSON.stringify(payload) : requestUrl = self.generateGetUrl(requestUrl, payload);
+            config.method.toLowerCase() === 'post' ? requestConfig.body = JSON.stringify(payload) : requestUrl = self.generateGetUrl(requestUrl, payload);
             var fetchPromise = fetch(requestUrl, requestConfig);
             fetchPromise
                 .then(function (response) {
