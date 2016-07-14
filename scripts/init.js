7c18cb58d8432f721d88adff518faedc7fe5faaf
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-07-15 05:15:07 +0530

    update

--- scripts/init.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by ravi.hamsa on 6/23/16.
- */
-ReactDOM.render(
-    React.createElement(window.App, {name: "World", context:window.appContext}),
-    document.getElementById('app')
-);
