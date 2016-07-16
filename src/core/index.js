1be9f14e9995bc8f04eb93db752201e7feebff82
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-07-16 21:49:53 +0530

    expose loader and message stack

--- src/core/index.js
+++ src/core/index.js
@@ -5,8 +5,10 @@
 import dataLoader from './dataLoader'
 import SimpleModel from './SimpleModel'
 import SmartWrapper from './SmartWrapper'
+import Loader from './Loader'
+import MessageStack from './MessageStack'
 import utils from './utils'
 import navigate from './navigate'
 import history from './history'
 
-export default {dataLoader, SimpleModel, SmartWrapper, utils, navigate, history}
+export default {dataLoader, SimpleModel, SmartWrapper,Loader, MessageStack, utils, navigate, history}
