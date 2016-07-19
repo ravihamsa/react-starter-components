c3bf9f2aa73cd7e5eb26bd3fdd3a45417f1b66c6
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-07-19 19:29:06 +0530

    switched to isomorphic history

--- src/core/history.js
+++ src/core/history.js
@@ -2,6 +2,10 @@
  * Created by ravi.hamsa on 7/14/16.
  */
 
-import { createHistory } from 'history'
+import createHistory from 'history/lib/createBrowserHistory';
+import createMemoryHistory from 'history/lib/createMemoryHistory';
+import useQueries from 'history/lib/useQueries';
+
+const history = useQueries(process.env.BROWSER ? createHistory : createMemoryHistory)();
 
 export default createHistory();
