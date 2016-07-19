652f2b59306cb8bbf3b3b7978cda9ea7b7f0a522
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-07-19 19:44:03 +0530

    excluded navigate for isomorphic reasons

--- src/core/index.js
+++ src/core/index.js
@@ -8,7 +8,7 @@ import SmartWrapper from './SmartWrapper'
 import Loader from './Loader'
 import MessageStack from './MessageStack'
 import utils from './utils'
-import navigate from './navigate'
+// import navigate from './navigate'
 // import history from './history'
 
-export default {dataLoader, SimpleModel, SmartWrapper,Loader, MessageStack, utils, navigate, history}
+export default {dataLoader, SimpleModel, SmartWrapper,Loader, MessageStack, utils}
