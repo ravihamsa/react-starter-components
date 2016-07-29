89a1b328f4834cd78a8555b274634c42bd9045ef
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-07-29 18:24:00 +0530

    added fileInput

---
+++ src/components/Form/FileInput.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ravi.hamsa on 6/29/16.
+ */
+
+import React, {PropTypes, Component} from "react";
+import FormElement from './FormElement'
+
+
+class FileInput extends FormElement {
+    render() {
+
+        let defaultValue = this.getDefaultValue();
+        let formClasses = this.getFormClasses();
+        let value = defaultValue || '';
+
+        return <fieldset className={formClasses}>
+            {this.props.showLabel ? <label>{this.props.label}</label> : null}
+            <input type={this.props.type} className="form-control" name={this.props.name}
+                   placeholder={this.props.placeholder} onChange={this.onChange.bind(this)}
+                   value={value} ref="input"/>
+            {this.props.helperText ? <small className="text-muted">{this.props.helperText}</small> : '' }
+            {this.props.errors ? <small className="text-muted">{this.props.errors}</small> : '' }
+        </fieldset>
+    }
+
+    getFileData(){
+        this.refs.input.getFileData();
+    }
+}
+
+
+export default FileInput;
