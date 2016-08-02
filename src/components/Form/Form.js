b8917736e9f69ecbbf79866bb85b4a8c5eee8e73
Ravi Hamsa <ravi.hamsa@inmobi.com>
2016-08-02 18:12:49 +0530

    validation rules added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,6 +10,7 @@ class Form extends Component {
     constructor() {
         super(...arguments);
         this._valueChangeHandler = this.onValueChange.bind(this);
+        this._errorHandler = this.onError.bind(this);
     }
 
     onSubmitHandler(event){
@@ -32,6 +33,12 @@ class Form extends Component {
         }
     }
 
+    onError(error){
+        if(this.props.onError){
+            this.props.onError(error)
+        }
+    }
+
     getChildContext() {
 
         if(!this.store){
@@ -42,10 +49,21 @@ class Form extends Component {
             }
             this._unsubscribeChange = store.on('change', this._valueChangeHandler);
             store.detailStore = detailStore;
+
+
+            let errorStore = this.errorStore = this.props.errorStore || new SimpleModel();
+            if(this._unsubscribeErrorChange){
+                this._unsubscribeErrorChange();
+            }
+
+            this._unsubscribeErrorChange = errorStore.on('change', this._errorHandler);
         }
+
+
 
         return {
             valueStore: this.store,
+            errorStore: this.errorStore,
             valueDetailStore:this.store.detailStore
         }
 
@@ -54,6 +72,7 @@ class Form extends Component {
 
 Form.childContextTypes = {
     valueStore: PropTypes.object.isRequired,
+    errorStore: PropTypes.object.isRequired,
     valueDetailStore: PropTypes.object.isRequired
 }
 
--- src/components/Form/FormElement.js
+++ src/components/Form/FormElement.js
@@ -2,6 +2,85 @@
  * Created by ravi.hamsa on 6/29/16.
  */
 import React, {PropTypes, Component} from "react";
+import _ from 'lodash';
+
+
+
+let validatorMap = {
+    'req': function(rule, value) {
+        return !_.isEmpty(value);
+    },
+    'digits': function(rule, value) {
+        return (/^\d{5}$/).test(value);
+    },
+    'alphanumeric': function(rule, value) {
+        var ck_alphaNumeric = /^\w+$/;
+        return ck_alphaNumeric.test(value);
+    },
+    'number': function(rule, value) {
+        if (value === undefined) {
+            return true;
+        }
+        var numberVal = +value;
+        return numberVal === numberVal;
+    },
+    'email': function(rule, value) {
+        var ck_email = /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-\\+]+)*@[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-\+]+)*(\.[A-Za-z]{2,})$/i;
+        return ck_email.test($.trim(value));
+    },
+    'minlen': function(rule, value) {
+        var min = rule.length;
+        return $.trim(String(value)).length >= min;
+    },
+    'maxlen': function(rule, value) {
+        var max = rule.length;
+        return $.trim(String(value)).length <= max;
+    },
+    'lt': function(rule, value) {
+        var target = parseFloat(rule.value);
+        var curvalue = parseFloat(value);
+        return curvalue < target;
+    },
+    'gt': function(rule, value) {
+        var target = parseFloat(rule.value);
+        var curvalue = parseFloat(value);
+        return curvalue > target;
+    },
+    'eq': function(rule, value) {
+        return rule.value === value;
+    },
+    'neq': function(rule, value) {
+        return rule.value !== value;
+    },
+    'url': function(rule, value) {
+        if (value === '') {
+            return true;
+        }
+        var ck_url = /(http|https|market):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i;
+        return ck_url.test($.trim(value));
+    },
+    'emaillist': function(rule, value) {
+        var emails = value.split(',');
+        var ck_email = /^([\w\-]+(?:\.[\w\-]+)*)@((?:[\w\-]+\.)*\w[\w\-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        for (var i = 0; i < emails.length; i++) {
+            if ($.trim(emails[i]) !== '' && !ck_email.test($.trim(emails[i]))) {
+                return false;
+            }
+        }
+        return true;
+    },
+    'function': function(rule, value) {
+        var func = rule.func;
+        return func.call(this, value, rule);
+    }
+}
+
+let getRuleValue = function(type){
+    return  {
+        type:type,
+        func:validatorMap[type]
+    }
+}
 
 class FormElement extends Component {
 
@@ -12,7 +91,16 @@ class FormElement extends Component {
         if(this.props.options){
             this.context.valueDetailStore.set({[name]: this.props.options.find((item)=> item.id===value)})
         }
-        this.setState({defaultValue:value})
+        let errors = this.validateValue(value);
+        this.setState({defaultValue:value, error:errors[0]})
+    }
+
+    validateValue(value){
+        let name = this.props.name;
+        let errors = this.validations.filter(function(item){
+            return item.func(value) === false;
+        })
+        return errors;
     }
 
     getValueFromNode(node){
@@ -20,6 +108,11 @@ class FormElement extends Component {
     }
 
     componentWillMount(){
+        let validations = this.props.validations || [];
+        this.validations = validations.forEach(function(rule, index){
+            return getRuleValue(rule);
+        });
+
         if(this.props.defaultValue){
             this.context.valueStore.set({[this.props.name]: this.props.defaultValue})
         }
@@ -41,6 +134,7 @@ class FormElement extends Component {
 FormElement.contextTypes = {
     valueStore: PropTypes.object.isRequired,
     valueDetailStore: PropTypes.object.isRequired,
+    errorStore: PropTypes.object.isRequired,
 }
 
 FormElement.propTypes = {
@@ -57,7 +151,6 @@ FormElement.defaultProps = {
     placeholder: 'Enter Text',
     label: 'Text Input',
     showLabel:true
-
 }
 
 
