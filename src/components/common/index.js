f8ee1c30e6037655c9731a1a15e0a9c1112618a9
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2016-08-27 07:10:04 +0530

    updates to List Component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,9 +2,9 @@
  * Created by ravi.hamsa on 6/29/16.
  */
 
-import List from './List';
+import List,{ListItem} from './List';
 import ViewStateManager, {ViewState} from './ViewStateManager';
 import FormCollection from './FormCollection';
 import InlinePopupGroup from './InlinePopupGroup';
 
-export default {List, ViewState, ViewStateManager, FormCollection, InlinePopupGroup}
+export default {List,ListItem,ViewState, ViewStateManager, FormCollection, InlinePopupGroup}
