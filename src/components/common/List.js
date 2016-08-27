cb12df46488f054664d6678e7db8533a32034627
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2016-08-27 06:43:23 +0530

    updates to List Component

--- src/components/common/List.js
+++ src/components/common/List.js
@@ -2,31 +2,43 @@
  * Created by ravi.hamsa on 6/30/16.
  */
 import React, {Component, PropTypes} from "react";
+import _ from 'lodash';
 
 class ListItem extends Component {
     render(){
         var itemData = this.props.itemData;
-        var ContainerTag = 'li';
-        return (<ContainerTag {...this.props} className="list-item">{itemData.name}</ContainerTag>);
+        var ContainerTag = this.props.tagName
+        var tagProps = _.pick(this.props, 'className', 'style')
+        return (<ContainerTag {...tagProps} className="list-item">{itemData.name}</ContainerTag>);
     }
 }
 
 
 class List extends Component {
+
+
+
     render() {
         var self = this;
         var itemArray = self.props.items;
         var ContainerTag = self.props.tagName || 'ul';
         var noItemMessage = self.props.noDataMessage || 'No Items Found'
         var ListItemClass = self.props.ListItem || ListItem;
+
+        var tagProps = _.pick(this.props, 'className', 'style')
+        var otherProps = _.omit(this.props, 'className', 'style', 'tagName', 'noDataMessage', 'ListItem', 'items');
+        otherProps.tagName = this.props.itemTagName || 'li';
+        otherProps.className = this.props.itemClassName || 'list-item';
+
+
         var listItems = itemArray.map(function (item) {
-            return <ListItemClass key={item.id} id={item.id} itemData={item} {...self.props}/>
+            return <ListItemClass key={item.id} id={item.id} itemData={item} {...otherProps}/>
         });
 
         if (listItems.length > 0) {
-            return (<ContainerTag {...this.props}>{listItems}</ContainerTag>);
+            return (<ContainerTag {...tagProps}>{listItems}</ContainerTag>);
         } else {
-            return (<ContainerTag {...this.props}><li className="no-data">{noItemMessage}</li></ContainerTag>)
+            return (<ContainerTag {...tagProps}><li className="no-data">{noItemMessage}</li></ContainerTag>)
         }
 
     }
