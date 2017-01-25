64fef2941475fff876a0bf7b02aeb12d5a3cf42b
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2017-01-26 00:55:08 +0530

    version bump

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@
 
 
 
+import coreIm from './core';
+import componentsIm from './components';
+
 export core from './core';
 export components from './components';
 
-export const {dataLoader, SimpleModel, SimpleEmitter, SmartWrapper, Loader, MessageStack, utils, rxutils} = core;
+export const {dataLoader, SimpleModel, SimpleEmitter, SmartWrapper, Loader, MessageStack, utils, rxutils} = coreIm;
 
-export const {Form, FormElement, TextInput, TextArea, FileInput, Select, CheckBox, HiddenInput, AutoComplete, DatePicker} = components.Form;
+export const {Form, FormElement, TextInput, TextArea, FileInput, Select, CheckBox, HiddenInput, AutoComplete, DatePicker} = componentsIm.Form;
 
-export const {PaginatedTable, Table, THEAD, TBODY, TH, TD, TR} = components.Table;
+export const {PaginatedTable, Table, THEAD, TBODY, TH, TD, TR} = componentsIm.Table;
 
-export const {List, FormCollection,AnchorLink, ActionLink, ToggleActionButton} = components.common;
+export const {List, FormCollection,AnchorLink, ActionLink, ToggleActionButton} = componentsIm.common;
 
-export const {ViewStateManager, ViewState} = components.common.ViewStateManager;
+export const {ViewStateManager, ViewState} = componentsIm.common.ViewStateManager;
 
-export const {InlinePopup, InlineButton, InlineBody} = components.common.InlinePopupGroup;
+export const {InlinePopup, InlineButton, InlineBody} = componentsIm.common.InlinePopupGroup;
 
-export const {Popup, PopupButton, PopupBody} = components.common.PopupGroup
+export const {Popup, PopupButton, PopupBody} = componentsIm.common.PopupGroup
+
+
