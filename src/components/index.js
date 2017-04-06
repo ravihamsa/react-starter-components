9f2b644b82c74c3fe1cdc4b9bae716e3d9f04b27
Ravi Hamsa <ravikumar.anantha@yahoo.in>
2017-04-06 15:11:55 +0530

    RXElementGroup added

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@
  */
 
 import Form from './Form';
-import {RXForm, RXSelect, RXFormElement, RXTextInput, RXTextArea, RXRadioList,RXCheckList,RXSelectionElement,RXDropdown,RXCheckbox} from './RXForm';
+import {RXForm, RXSelect, RXElementGroup, RXFormElement, RXTextInput, RXTextArea, RXRadioList,RXCheckList,RXSelectionElement,RXDropdown,RXCheckbox} from './RXForm';
 import Table from './Table';
 import common from './common';
 
